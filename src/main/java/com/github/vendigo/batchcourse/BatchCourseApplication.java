468fb3fdbef36223d4eb347c4e971c5e2166b4ed
Dmytro Marchenko <dmmarchenko@corelogic.com>
2020-06-25 11:01:15 +0300

    End of course

--- src/main/java/com/github/vendigo/batchcourse/BatchCourseApplication.java
+++ src/main/java/com/github/vendigo/batchcourse/BatchCourseApplication.java
@@ -11,5 +11,4 @@ public class BatchCourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BatchCourseApplication.class, args);
     }
-
 }
--- src/main/java/com/github/vendigo/batchcourse/config/FlowerBatchConfiguration.java
+++ src/main/java/com/github/vendigo/batchcourse/config/FlowerBatchConfiguration.java
@@ -1,32 +1,90 @@
 package com.github.vendigo.batchcourse.config;
 
+import java.util.Date;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import com.github.vendigo.batchcourse.listener.FlowersSelectionStepExecutionListener;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @AllArgsConstructor
 @Slf4j
-public class FlowerBatchConfiguration {
+@EnableScheduling
+public class FlowerBatchConfiguration extends QuartzJobBean {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final FlowersSelectionStepExecutionListener selectFlowerListener;
     private final Flow deliveryFlow;
+    private final JobLauncher jobLauncher;
+    private final JobExplorer jobExplorer;
+
+    @Scheduled(cron = "0 0 * * * *")
+    @SneakyThrows
+    public void runJob() {
+        JobParametersBuilder paramBuilder = new JobParametersBuilder();
+        paramBuilder.addDate("run.date", new Date());
+        paramBuilder.addString("type", "roses");
+        jobLauncher.run(prepareFlowersJob(), paramBuilder.toJobParameters());
+    }
+
+    @Override
+    @SneakyThrows
+    protected void executeInternal(JobExecutionContext jobExecutionContext) {
+        JobParameters parameters = new JobParametersBuilder(jobExplorer)
+            .getNextJobParameters(prepareFlowersJob())
+            .toJobParameters();
+        jobLauncher.run(prepareFlowersJob(), parameters);
+    }
+
+    @Bean
+    public Trigger trigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder
+            .simpleSchedule()
+            .withIntervalInSeconds(30)
+            .repeatForever();
+        return TriggerBuilder.newTrigger()
+            .forJob(jobDetail())
+            .withSchedule(scheduleBuilder)
+            .build();
+    }
+
+    @Bean
+    public JobDetail jobDetail() {
+        return JobBuilder.newJob(FlowerBatchConfiguration.class)
+            .storeDurably()
+            .build();
+    }
 
     @Bean
     public Job prepareFlowersJob() {
         return jobBuilderFactory.get("prepareFlowers")
+            .incrementer(new RunIdIncrementer())
             .start(selectFlowersStep())
             .on("TRIM REQUIRED").to(removeThornsStep())
             .next(arrangeFlowersStep())
